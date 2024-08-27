5903ddd885eede4f812fdebe1d5bd3cb57125d2b
Stan <moscarstanley37@gmail.com>
2024-08-26 21:11:12 -0600

    js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 //Extraer e importar las dependencias
-import {src, dest, watch } from "gulp"
+import {src, dest, watch, series } from "gulp"
 import * as dartSass from "sass"
 import gulpSass from 'gulp-sass'
 
 //Crear una función que le dice a gulp que utilice las funciones de sass
 const sass = gulpSass(dartSass)
+
+export function js(done) {
+
+    src("src/js/app.js")
+        .pipe(dest("build/js"))
+    
+    done()
+}
 
 //Funcion que compila el sass
 export function css(done){
@@ -19,5 +27,8 @@ export function css(done){
 //Funcion que se ejecuta y observa el archivo sass
 export function dev() {
     watch("src/scss/**/*.scss", css ) 
+    watch("src/js/**/*.js", js ) 
     //Archivo que vas a obtener y funcion que ejecuta el código
 }
+
+export default series(js,css,dev)
